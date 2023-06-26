23f548a731fcb485c2871353c25f526b8d86a4d1
Dillon Ellis <dillonzellis@gmail.com>
2023-06-26 12:31:54 -0400

    add to innercontent component to have an order variant

--- app/(components)/inner-pages/InnerContent.tsx
+++ app/(components)/inner-pages/InnerContent.tsx
@@ -1,29 +1,64 @@
 /* eslint-disable @next/next/no-img-element */
 import Container from "@/app/(layout)/Container";
-import ContentSection from "@/app/(layout)/ContentSection";
 import TwoCol from "@/app/(layout)/TwoCol";
 import ContentCol from "./ContentCol";
+
+type BgColorVariant = "white" | "blue" | "green";
+type OrderVariant = "imageFirst" | "contentFirst";
 
 type InnerContentProps = {
   children: React.ReactNode;
   ImgSrc: string;
   ImgAlt: string;
+  variant: BgColorVariant;
+  order: OrderVariant;
 };
 
-const InnerContent = ({ children, ImgSrc, ImgAlt }: InnerContentProps) => {
+const InnerContent = ({
+  children,
+  ImgSrc,
+  ImgAlt,
+  variant,
+  order,
+}: InnerContentProps) => {
+  const bgColorClassMap: Record<BgColorVariant, string> = {
+    white: "bg-white",
+    blue: "bg-brandingBlue-400",
+    green: "bg-brandingGreen-400",
+  };
+
+  const bgClass = bgColorClassMap[variant];
+
   return (
-    <div>
-      <ContentSection>
-        <Container>
-          <TwoCol>
-            <div>
-              <img className="rounded-md shadow-lg" src={ImgSrc} alt={ImgAlt} />
-            </div>
-            <ContentCol>{children}</ContentCol>
-          </TwoCol>
-        </Container>
-      </ContentSection>
-    </div>
+    <section className={`py-8 lg:py-16 ${bgClass}`}>
+      <Container>
+        <TwoCol>
+          {order === "contentFirst" ? (
+            <>
+              <ContentCol>{children}</ContentCol>
+              <div>
+                <img
+                  className="rounded-md shadow-lg"
+                  src={ImgSrc}
+                  alt={ImgAlt}
+                />
+              </div>
+            </>
+          ) : (
+            <>
+              <div>
+                <img
+                  className="rounded-md shadow-lg"
+                  src={ImgSrc}
+                  alt={ImgAlt}
+                />
+              </div>
+              <ContentCol>{children}</ContentCol>
+            </>
+          )}
+        </TwoCol>
+      </Container>
+    </section>
   );
 };
 
