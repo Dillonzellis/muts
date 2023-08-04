e238781e502563e8fd4fa84cd261ff0e83a0e7a2
dillonzellis <dillonzellis@gmail.com>
2023-08-04 13:20:36 -0400

    add inner page hero

---
+++ app/accounts/page.tsx
@@ -0,0 +1,35 @@
+import HeroImg from "../components/inner-pages/HeroImg";
+
+const page = () => {
+  return (
+    <main>
+      <HeroImg
+        title="accounts"
+        desc="Save & Earn Accounts"
+        desc2="Dividend bearing accounts promote financial growth"
+        imgSrc="http://dev2.growthbydesign.org/wp-content/uploads/account-hero.jpg"
+        imgAlt="accounts"
+      />
+      {/* <Container> */}
+      {/*   <ContentSection> */}
+      {/*     <h2 className="text-5xl font-bold text-brandingBlue-400 pb-2"> */}
+      {/*       Savings */}
+      {/*     </h2> */}
+      {/*     <div className="text-lg lg:text-2xl">Save & Earn Accounts</div> */}
+      {/*     <div className="lg:text-lg italic"> */}
+      {/*       Dividend bearing accounts promote financial growth */}
+      {/*     </div> */}
+      {/*   </ContentSection> */}
+      {/* </Container> */}
+      {/* <RegSavings /> */}
+      {/* <MMAccounts /> */}
+      {/* <ClubAccounts /> */}
+      {/* <Cds /> */}
+      {/* <IRAs /> */}
+      {/* <CoverdaleAccounts /> */}
+      <div className="py-4"></div>
+    </main>
+  );
+};
+
+export default page;
