e09134b23ffa1f3bfbaaf3a594fb5258002920ec
dillonzellis <dillonzellis@gmail.com>
2023-08-07 22:25:21 -0400

    make club accts two col

--- app/accounts/(savings)/clubAccounts/ClubAccounts.tsx
+++ app/accounts/(savings)/clubAccounts/ClubAccounts.tsx
@@ -4,50 +4,46 @@ import ContentSecondaryHeading from "@/app/components/inner-pages/ContentSeconda
 import React from "react";
 import Button from "@/app/components/Button";
 import List from "@/app/components/List";
-import InnerContent from "@/app/components/inner-pages/InnerContent";
 import { items, items2 } from "./items";
+import ContentTwoCol from "@/app/components/inner-pages/ContentTwoCol";
+import TwoCol from "@/app/layouts/TwoCol";
+import ContentCol from "@/app/components/inner-pages/ContentCol";
 
 const ClubAccounts = () => {
   return (
-    <InnerContent
-      sectionName="club-accts"
-      order="imageFirst"
-      variant="white"
-      ImgSrc="http://dev2.growthbydesign.org/wp-content/uploads/iStock-1420037581.jpg"
-      ImgAlt="savings account">
-      <div>
-        <div className="tw-flex tw-flex-col tw-gap-3">
-          <ContentHeading title="Club Accounts " />
-        </div>
-        <div className="tw-flex tw-flex-col tw-gap-2">
-          <ContentBody>
-            Save a little at a time throughout the year for a specific purpose
-            so you can reap the benefits of your hard work when the need rolls
-            around!
-          </ContentBody>
+    <ContentTwoCol sectionName="club-accts" variant="white">
+      <div className="tw-flex tw-flex-col tw-gap-3">
+        <ContentHeading title="Club Accounts " />
+        <ContentBody>
+          Save a little at a time throughout the year for a specific purpose
+          so you can reap the benefits of your hard work when the need rolls
+          around!
+        </ContentBody>
+      </div>
+      <TwoCol>
+        <ContentCol>
           <ContentSecondaryHeading title="Holiday Club Accounts" />
           <div>
             Prepare for the expensive holiday shopping season by saving
             throughout the year.
           </div>
           <List liItems={items} />
-        </div>
-      </div>
-      <div className="tw-flex tw-flex-col tw-gap-2 lg:tw-flex-row tw-pb-2">
-        <Button btnText="Open an Account" link="#" />
-        <Button btnText="calculate savings" link="#" />
-      </div>
-
-      <div className="tw-flex tw-flex-col tw-gap-2">
-        <ContentSecondaryHeading title="Vacation Club Accounts" />
-        <div>Stash funds away for a vacation getaway!</div>
-        <List liItems={items2} />
-      </div>
-      <div className="tw-flex tw-flex-col tw-gap-2 lg:tw-flex-row">
-        <Button btnText="Open an Account" link="#" />
-        <Button btnText="calculate savings" link="#" />
-      </div>
-    </InnerContent>
+          <div className="tw-flex tw-flex-col tw-gap-2 lg:tw-flex-row tw-pb-2">
+            <Button btnText="Open an Account" link="#" />
+            <Button btnText="calculate savings" link="#" />
+          </div>
+        </ContentCol>
+        <ContentCol>
+          <ContentSecondaryHeading title="Vacation Club Accounts" />
+          <div>Stash funds away for a vacation getaway!</div>
+          <List liItems={items2} />
+          <div className="tw-flex tw-flex-col tw-gap-2 lg:tw-flex-row">
+            <Button btnText="Open an Account" link="#" />
+            <Button btnText="calculate savings" link="#" />
+          </div>
+        </ContentCol>
+      </TwoCol>
+    </ContentTwoCol>
   );
 };
 
