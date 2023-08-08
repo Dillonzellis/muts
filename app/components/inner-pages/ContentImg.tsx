5dea9a545f76f494da987a7d707ccd19dc4d9402
dillonzellis <dillonzellis@gmail.com>
2023-08-08 09:22:40 -0400

    add shadow and rounded egdes to images

--- app/components/inner-pages/ContentImg.tsx
+++ app/components/inner-pages/ContentImg.tsx
@@ -8,7 +8,7 @@ type ContentImgProps = {
 const ContentImg = ({ ImgSrc, ImgAlt, order }: ContentImgProps) => {
   return (
     <>
-      <img className={`tw-w-full tw-max-w-[503px] tw-mx-auto tw-rounded-md tw-shadow-lg ${order}`} src={ImgSrc} alt={ImgAlt} />
+      <img className={`tw-w-full tw-max-w-[503px] tw-mx-auto tw-rounded-2xl tw-shadow-2xl tw-shadow-brandingGrey-700/50 ${order}`} src={ImgSrc} alt={ImgAlt} />
     </>
   );
 };
