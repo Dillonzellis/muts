e88569b6928bf32f982bc9c9062881f02cfd36a9
dillonzellis <dillonzellis@gmail.com>
2023-08-08 15:20:44 -0400

    add size props to Container

--- app/layouts/Container.tsx
+++ app/layouts/Container.tsx
@@ -1,10 +1,17 @@
 type ContainerProps = {
   children: React.ReactNode;
+  size?: "small" | "large"
 }
 
-const Container = ({ children }: ContainerProps) => {
+const Container = ({ children, size }: ContainerProps) => {
+
+  let maxWidth = 'tw-max-w-[1180px]';
+  if (size === "small") maxWidth = 'tw-max-w-[850px]'
+  if (size === "large") maxWidth = 'tw-max-w-[1400px]'
+
+
   return (
-    <div className="tw-container tw-mx-auto tw-max-w-[1180px] tw-px-6 md:tw-px-12">{children}</div>
+    <div className={`tw-container tw-mx-auto ${maxWidth} tw-px-6 md:tw-px-12`}>{children}</div>
   )
 }
 
