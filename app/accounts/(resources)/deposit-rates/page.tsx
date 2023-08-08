53487066e32b1e4c809084e3307e279c86b948de
dillonzellis <dillonzellis@gmail.com>
2023-08-08 14:33:16 -0400

    add rest of share table

--- app/accounts/(resources)/deposit-rates/page.tsx
+++ app/accounts/(resources)/deposit-rates/page.tsx
@@ -2,7 +2,8 @@ import DataTable from "@/app/components/DataTable";
 import PageHeader from "@/app/components/inner-pages/PageHeader";
 import SectionContent from "@/app/components/inner-pages/SectionContent";
 import Container from "@/app/layouts/Container";
-import { shareHeaders, shareData } from "./share-acct-data";
+import { shareHeaders, shareData } from "./rateData";
+import Disclaimer from "@/app/components/Disclaimer";
 
 const DepositRates = () => {
   return (
@@ -14,7 +15,10 @@ const DepositRates = () => {
             <div className="tw-text-xl tw-pb-2">Share Rates $5 minimum investment</div>
             <div>Annual Percentage Yield, rates and other terms may change at any time without prior notice. A penalty is imposed for early withdrawal. Fees could reduce earnings on the account.</div>
           </div>
-          <DataTable headers={shareHeaders} data={shareData} />
+          <div className="tw-max-w-[850px] tw-mx-auto tw-flex tw-flex-col tw-gap-2">
+            <DataTable headers={shareHeaders} data={shareData} />
+            <Disclaimer text="*Dividends will be compounded quarterly and will be credited quarterly. Average Daily Balance." />
+          </div>
         </div>
       </Container>
     </SectionContent>
---
+++ app/accounts/(resources)/deposit-rates/rateData.ts
@@ -0,0 +1,50 @@
+export const shareHeaders = [
+  { key: 'accountType', label: 'Account Type', className: 'tw-rounded-tl-xl' },
+  { key: 'frequency', label: 'Frequency' },
+  { key: 'dividendRate', label: 'Dividend Rate' },
+  { key: 'annualPercentageYield', label: 'Annual Percentage Yield', className: 'tw-rounded-tr-xl' }
+];
+
+
+export const shareData = [
+  {
+    accountType: 'Regular Share Account',
+    frequency: 'Quarterly',
+    dividendRate: '0.05%',
+    annualPercentageYield: '0.05%'
+  },
+  {
+    accountType: 'Checking',
+    frequency: 'Monthly',
+    dividendRate: '0.05%',
+    annualPercentageYield: '0.05%'
+  },
+  {
+    accountType: 'Money Market Account (Under $100,000)',
+    frequency: 'Monthly',
+    dividendRate: '0.40%',
+    annualPercentageYield: '0.401%'
+  },
+  {
+    accountType: 'Money Market Account (Over $100,000)',
+    frequency: 'Monthly',
+    dividendRate: '0.75%',
+    annualPercentageYield: '0.753%'
+  },
+  {
+    accountType: 'Christmas & Vacation Club',
+    frequency: 'Quarterly',
+    dividendRate: '0.05%',
+    annualPercentageYield: '0.753%'
+  },
+  {
+    accountType: 'IRA Savings',
+    frequency: 'Quarterly',
+    dividendRate: '0.05%',
+    annualPercentageYield: '0.753%'
+  },
+];
+
+
+
+
