a4d6ed06d1273eb41f34b948f1c65843a3679879
dillonzellis <dillonzellis@gmail.com>
2023-08-09 16:11:47 -0400

    add fee schedule data

--- app/accounts/(resources)/fee-schedule/fee-data.ts
+++ app/accounts/(resources)/fee-schedule/fee-data.ts
@@ -1,12 +1,139 @@
 export const feeHeaders = [
-  { key: 'fee', label: 'Fees' }
+  {
+    key: 'fee',
+    label: 'Mutual Savings Cu Fee Schedule - 1/01/21',
+    className: 'tw-rounded-t-xl'
+  }
 ];
 
 export const feeData = [
   {
-    fee: 'membership fee',
-    test: 'test',
-    value: '5.00'
+    fee: 'Nonsufficient Funds (NSF)',
+    amount: '$30.00 per item',
+  },
+  {
+    fee: 'Courtesy Pay Fee',
+    amount: '$30.00 per item',
+  },
+  {
+    fee: 'Stop Payments',
+    amount: '$25.00 per item',
+  },
+  {
+    fee: 'Copy of Paid Drafts',
+    amount: '$3.00 per copy',
+  },
+  {
+    fee: 'ACH Stop Payment',
+    amount: '$25.00 per item',
+  },
+  {
+    fee: 'Return Deposit Items',
+    amount: '15.00 per item',
+  },
+  {
+    fee: 'ATM Transactions',
+    amount: 'Free*',
+  },
+]
+
+export const mmHeaders = [
+  {
+    key: 'fee',
+    label: 'Money Market Account Fees',
+    className: 'tw-rounded-t-xl'
   }
+]
+
+export const mmData = [
+  {
+    fee: 'All other fees applicable to Deposit Accounts (example: NSF, Stop Payments, Returned Deposits, etc.)'
+  }
+]
+
+export const webHeaders = [
+  {
+    key: 'fee',
+    label: "Web Bill Pay Fees",
+    className: 'tw-rounded-t-xl'
+  }
+]
+
+export const webData = [
+  {
+    fee: 'NSF/Overdraft',
+    amount: '$30.00 per item',
+  },
+  {
+    fee: 'Card Replacement Fee',
+    amount: '$25.00 per item',
+  },
+  {
+    fee: 'Bill Pay "Rush Delivery"',
+    amount: '$9.95 per item',
+  },
+]
+
+export const otherHeaders = [
+  {
+    key: 'fee',
+    label: 'Other Services Fees(Applicable to all Accounts)',
+    className: 'tw-rounded-t-xl'
+  }
+]
+
+export const otherData = [
+  {
+    fee: 'Account Reconciliation',
+    amount: '$20.00 per hour'
+  },
+  {
+    fee: 'Card Replacement Fee',
+    amount: '1 Free per year; $5.00 per card thereafter',
+  },
+  {
+    fee: 'Rushed Debit Cards',
+    amount: '$35.00 ',
+  },
+  {
+    fee: 'Official Checks',
+    amount: '$3.00 per check',
+  },
+  {
+    fee: 'Express Mail Fee',
+    amount: 'At Cost',
+  },
+  {
+    fee: 'Gift Card Fee',
+    amount: '$2.50 each',
+  },
+  {
+    fee: 'Credit Union Checks',
+    amount: '$1.00 per Check',
+  },
+  {
+    fee: 'Money Orders',
+    amount: '$1.00 per Money Order',
+  },
+  {
+    fee: 'Research Fee',
+    amount: '$20.00 per hours',
+  },
+  {
+    fee: 'Statement Copy',
+    amount: '$2.00 per hard copy',
+  },
+  {
+    fee: 'Incoming Domestic Wire',
+    amount: '$2.00 per wire',
+  },
+  {
+    fee: 'Outgoing Domestic Wire',
+    amount: '$15.00 per wire',
+  },
+  {
+    fee: 'Garnishment & Levy Processing Fee',
+    amount: '10% of account balance up to $50.00 maximum',
+  },
 ]
 
--- app/accounts/(resources)/fee-schedule/page.tsx
+++ app/accounts/(resources)/fee-schedule/page.tsx
@@ -1,24 +1,36 @@
-import { feeHeaders, feeData } from "./fee-data";
-import DataTable2 from "@/app/components/DataTable2";
+import SectionContent from "@/app/components/inner-pages/SectionContent";
+import { feeHeaders, feeData, mmHeaders, mmData, webHeaders, webData } from "./fee-data";
+import DataTable from "@/app/components/DataTable";
+import Container from "@/app/layouts/Container";
+import Disclaimer from "@/app/components/Disclaimer";
+import SectionHeaders from "@/app/components/inner-pages/SectionHeaders";
+import PageHeader from "@/app/components/inner-pages/PageHeader";
 
 const FeeSchedule = () => {
   return (
-    <div>
-
-      <DataTable2
-        headers={[
-          { key: 'accountType', label: 'Account Type' },
-        ]}
-        data={[
-          {
-            accountType: 'Regular Share Account',
-            frequency: 'Quarterly',
-            dividendRate: '0.05%',   // This column won't have a header
-            annualPercentageYield: '0.05%'  // This column won't have a header
-          }
-        ]}
-      />
-    </div>
+    <SectionContent sectionName="fee-tables">
+      <Container>
+        <PageHeader title="Fee Schedule" />
+        <div className="tw-flex tw-flex-col tw-gap-8">
+          <DataTable
+            headers={feeHeaders}
+            data={feeData}
+          />
+          <DataTable
+            headers={mmHeaders}
+            data={mmData}
+          />
+          <DataTable
+            headers={webHeaders}
+            data={webData}
+          />
+          <div>
+            <Disclaimer text="*Members will receive all ATM Transactions at no charge. CULIANCE and Publix Presto ATM transactions are permitted unlimited transactions at no additional charge." />
+            <Disclaimer text="The fees appearing in this Schedule are accurate and effective for member accounts as of the Effective Date on this Fee Schedule. If you have any questions or require current rate and fee information on your accounts, please contact a credit union representative for assistance." />
+          </div>
+        </div>
+      </Container>
+    </SectionContent >
   )
 }
 
