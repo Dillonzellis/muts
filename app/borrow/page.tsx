fcf479bc4a9fede6f3112984ee215f0c64771bc1
dillonzellis <dillonzellis@gmail.com>
2023-08-10 15:40:45 -0400

    add borrow hero

---
+++ app/borrow/page.tsx
@@ -0,0 +1,17 @@
+import HeroImg from "../components/inner-pages/HeroImg"
+
+const BorrowPage = () => {
+  return (
+    <div>
+      <HeroImg
+        title="Borrow"
+        desc="Borrow & Buy Accounts"
+        desc2="Competitive loans promote access & credit growth"
+        imgSrc="http://dev2.growthbydesign.org/wp-content/uploads/iStock-1393059175.jpg"
+        imgAlt=""
+      />
+    </div>
+  )
+}
+
+export default BorrowPage;
