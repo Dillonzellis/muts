ab18f2545b45b138d4a4283824d3cdd48f4f3e37
dillonzellis <dillonzellis@gmail.com>
2023-08-10 12:09:26 -0400

    add italic variant for contentSecondayHeading

--- app/components/inner-pages/ContentSecondaryHeading.tsx
+++ app/components/inner-pages/ContentSecondaryHeading.tsx
@@ -1,9 +1,15 @@
 type ContentSecondaryHeadingProps = {
   title: string;
+  italic?: boolean;
 };
 
-const ContentSecondaryHeading = ({ title }: ContentSecondaryHeadingProps) => {
-  return <h4 className="tw-text-xl tw-font-semibold">{title}</h4>;
+
+
+const ContentSecondaryHeading = ({ title, italic }: ContentSecondaryHeadingProps) => {
+
+  const italicClass = italic ? "tw-italic" : "";
+
+  return <div className={`tw-text-xl tw-font-semibold ${italicClass}`.trim()}>{title}</div>;
 };
 
 export default ContentSecondaryHeading;
