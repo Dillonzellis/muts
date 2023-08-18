5650f52409c18af7fece4f19221a18e80eeb4399
dillonzellis <dillonzellis@gmail.com>
2023-08-18 11:00:48 -0400

    fix apos error

--- app/borrow/gap/items.tsx
+++ app/borrow/gap/items.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const gapItems: React.ReactNode[] = [
   <React.Fragment key="1">Covers the difference between the actual cash value of your vehicle and the loan/lease balance, less delinquent payments, late charges, refundable service warranty contracts and other insurance-related charges.</React.Fragment>,
-  <React.Fragment key="2">Covers your auto insurance deductible of $500 or $1,000. The actual amount depends on your credit union's program limits. Ask your lender/loan officer for details.</React.Fragment>,
+  <React.Fragment key="2">Covers your auto insurance deductible of $500 or $1,000. The actual amount depends on your credit union&apos;s program limits. Ask your lender/loan officer for details.</React.Fragment>,
   <React.Fragment key="3">Is a loan/lease deficiency waiver and is not offered as insurance coverage.</React.Fragment>,
   <React.Fragment key="4">Purchase is voluntary and cannot be required as a condition of credit approval.</React.Fragment>,
 ];
