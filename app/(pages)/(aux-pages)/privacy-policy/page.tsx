5d74cb97098c61ee2304591417a393968c22edb7
dillonzellis <dillonzellis@gmail.com>
2023-08-22 15:49:55 -0400

    add privacy policy

---
+++ app/(pages)/(aux-pages)/privacy-policy/page.tsx
@@ -0,0 +1,18 @@
+
+import SectionContent from "@/app/components/inner-pages/SectionContent";
+import Container from "@/app/layouts/Container";
+import PageHeader from "@/app/components/inner-pages/PageHeader";
+
+const CalculatorIndex = () => {
+  return (
+    <SectionContent>
+      <Container>
+        <PageHeader title="Privacy Policy" />
+        {/* <div className="tw-text-xl md:tw-text-xl tw-italic tw-pb-2 md:tw-pb-8 tw-text-black">Calculate your savings, monthly payments & more!</div> */}
+        <div>Privacy Policy placeholder</div>
+      </Container>
+    </SectionContent>
+  )
+}
+
+export default CalculatorIndex;
