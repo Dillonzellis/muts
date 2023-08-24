a7557747f34c989c688b5ed1c686a755b34c4cfa
dillonzellis <dillonzellis@gmail.com>
2023-08-24 16:44:04 -0400

    add modal and testing page

---
+++ app/(pages)/testing-page/page.tsx
@@ -0,0 +1,28 @@
+import Modal from "@/app/components/Modal";
+import HeroImg from "@/app/components/inner-pages/HeroImg"
+import SectionContent from "@/app/components/inner-pages/SectionContent";
+import Container from "@/app/layouts/Container";
+
+const TestingPage = () => {
+  return (
+    <div>
+      <HeroImg
+        title="Solutions"
+        desc="Convenient tools to ease everyday banking"
+        imgSrc="http://dev2.growthbydesign.org/wp-content/uploads/iStock-1435234222.jpg"
+        imgAlt=""
+      />
+      <Container>
+        <SectionContent>
+          <Modal
+            modalClass="test"
+            cancelLink=""
+            continueLink=""
+          />
+        </SectionContent>
+      </Container>
+    </div>
+  )
+}
+
+export default TestingPage; 
---
+++ app/components/Modal.tsx
@@ -0,0 +1,47 @@
+type ModalProps = {
+  modalClass: string
+  continueLink: string
+  cancelLink: string
+}
+
+type ButtonProps = {
+  children: React.ReactNode
+  cancel?: boolean
+  href: string
+}
+
+const Button = ({ children, cancel, href }: ButtonProps) => {
+
+  const variantClass = cancel ? "tw-border-2 tw-border-brandingBlue-400 tw-text-brandingBlue-400 tw-bg-white" : "tw-bg-brandingBlue-400 tw-text-white"
+
+  return (
+    <div className={`${variantClass} tw-transition tw-transform hover:tw-scale-105 tw-justify-center tw-font-semibold tw-rounded-md tw-inline-flex tw-cursor-pointer tw-items-center tw-h-10 tw-px-12 tw-py-2 tw-capitalize`}>
+      <a href={href}>{children}</a>
+    </div>
+  )
+}
+
+const Modal = ({ modalClass, continueLink, cancelLink }: ModalProps) => {
+  const cuName = "Mutual Savings Credit Union"
+
+  const containerPadding = "tw-px-3 md:tw-px-12"
+
+  return (
+    <div className={`${modalClass} tw-max-w-[662px] tw-border tw-rounded-md`}>
+      <div className={`${containerPadding} tw-pt-6 tw-pb-4`}>
+        <div className="tw-font-bold tw-text-xl tw-pb-4">Important Message</div>
+        <div className="tw-text-sm md:tw-text-base">
+          By accessing the noted link you will be leaving {cuName} website and entering a website hosted by another party. {cuName} has not approved this as a reliable partner site. Please be advised that you will no longer be subject to, or under the protection of, the privacy and security policies of {cuName} website. We encourage you to read and evaluate the privacy and security policies of the site you are entering, which may be different than those of {cuName}.
+        </div>
+      </div>
+      <div className="tw-bg-brandingBlue-100 tw-py-6">
+        <div className={`${containerPadding} tw-bg-brandingGrey-100 tw-flex tw-cursor-pointer tw-flex-col md:tw-flex-row tw-gap-2 md:tw-gap-4`}>
+          <Button cancel href={cancelLink}>Cancel</Button>
+          <Button href={continueLink}>Continue</Button>
+        </div>
+      </div>
+    </div >
+  )
+}
+
+export default Modal;
