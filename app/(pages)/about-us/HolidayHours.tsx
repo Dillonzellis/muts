82023a88252322702f9501659896aab4acfd01e5
dillonzellis <dillonzellis@gmail.com>
2023-08-24 16:43:23 -0400

    fix apos error and keys missing

--- app/(pages)/about-us/HolidayHours.tsx
+++ app/(pages)/about-us/HolidayHours.tsx
@@ -6,16 +6,16 @@ import InnerContent from "@/app/components/inner-pages/InnerContent"
 import ContentHeaderWrapper from "@/app/components/inner-pages/ContentHeaderWrapper";
 
 const liItems: React.ReactNode[] = [
-  <React.Fragment>New Year's Day – January 2, 2023</React.Fragment>,
-  <React.Fragment>Martin Luther King, Jr. Day – January 16, 2023</React.Fragment>,
-  <React.Fragment>Memorial Day – May 29, 2023</React.Fragment>,
-  <React.Fragment>Independence Day – July 4, 2023</React.Fragment>,
-  <React.Fragment>Labor Day – September 4, 2023</React.Fragment>,
-  <React.Fragment>Thanksgiving Holiday – November 23, 2023</React.Fragment>,
-  <React.Fragment>Thanksgiving Holiday – November 24, 2023</React.Fragment>,
-  <React.Fragment>Christmas Holiday – December 25, 2023</React.Fragment>,
-  <React.Fragment>Christmas Holiday – December 26, 2023</React.Fragment>,
-  <React.Fragment>New Year's Day – January 1, 2024</React.Fragment>,
+  <React.Fragment key="1">New Year&apos;s Day – January 2, 2023</React.Fragment>,
+  <React.Fragment key="2">Martin Luther King, Jr. Day – January 16, 2023</React.Fragment>,
+  <React.Fragment key="3">Memorial Day – May 29, 2023</React.Fragment>,
+  <React.Fragment key="4">Independence Day – July 4, 2023</React.Fragment>,
+  <React.Fragment key="5">Labor Day – September 4, 2023</React.Fragment>,
+  <React.Fragment key="6">Thanksgiving Holiday – November 23, 2023</React.Fragment>,
+  <React.Fragment key="7">Thanksgiving Holiday – November 24, 2023</React.Fragment>,
+  <React.Fragment key="8">Christmas Holiday – December 25, 2023</React.Fragment>,
+  <React.Fragment key="9">Christmas Holiday – December 26, 2023</React.Fragment>,
+  <React.Fragment key="10">New Year&apos;s Day – January 1, 2024</React.Fragment>,
 ];
 
 const HolidayHours = () => {
