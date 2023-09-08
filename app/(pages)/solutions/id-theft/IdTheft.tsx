c826b2f503940df9e17e1810a5f95e3bb890093d
dillonzellis <dillonzellis@gmail.com>
2023-09-08 16:43:41 -0400

    update solutions link

--- app/(pages)/solutions/id-theft/IdTheft.tsx
+++ app/(pages)/solutions/id-theft/IdTheft.tsx
@@ -1,5 +1,5 @@
 import ContentHeading from "@/app/components/inner-pages/ContentHeading";
-import InnerContent from "@/app/components/inner-pages/InnerContent"
+import InnerContent from "@/app/components/inner-pages/InnerContent";
 import LinkText from "@/app/components/LinkText";
 import ReportCols from "./ReportCols";
 
@@ -13,35 +13,52 @@ const IdTheft = () => {
       variant="blue"
     >
       <ContentHeading title="Report I.D. Theft" />
-      <div>If you suspect identity theft, contact us immediately as well as one of the three major credit bureaus to discuss whether you need to place a fraud alert on your file to prevent thieves from opening a new account in your name.</div>
+      <div>
+        If you suspect identity theft, contact us immediately as well as one of
+        the three major credit bureaus to discuss whether you need to place a
+        fraud alert on your file to prevent thieves from opening a new account
+        in your name.
+      </div>
       <div className="tw-flex-wrap tw-flex tw-gap-6">
-        <ReportCols title="Equifax" href="https://www.equifax.com/personal/">
+        <ReportCols title="Equifax" href="/#wow-modal-id-12">
           <div>
-            <LinkText label="800-525-6285" href="" />
+            <LinkText dark label="800-525-6285" href="tel:+1800-525-6285" />
           </div>
           <div>P.O. Box 740250</div>
           <div>Atlanta, GA 30374</div>
         </ReportCols>
 
-        <ReportCols title="Experian" href="https://www.experian.com/">
+        <ReportCols title="Experian" href="/#wow-modal-id-13">
           <div>
-            <LinkText label="888-397-3742" href="" />
+            <LinkText dark label="888-397-3742" href="tel:+1888-397-3742" />
           </div>
           <div>P.O. Box 1017</div>
           <div>Allen, TX 75013</div>
         </ReportCols>
 
-        <ReportCols title="TransUnion" href="https://www.transunion.com/">
+        <ReportCols title="TransUnion" href="/#wow-modal-id-14">
           <div>
-            <LinkText label="800-680-7289" href="" />
+            <LinkText dark label="800-680-7289" href="tel:+1800-680-7289" />
           </div>
           <div>P.O. Box 6790</div>
           <div>Fullerton, CA 92634</div>
         </ReportCols>
       </div>
-      <div><em>Report all suspicious contacts to the Federal Trade Commission by calling <LinkText label="1-877-IDTHEFT" href="" />. To learn more about identity theft protocol, visit the <LinkText label="FTC’s Identity Theft site" href="https://www.identitytheft.gov/" />.</em></div>
+      <div>
+        <em>
+          Report all suspicious contacts to the Federal Trade Commission by
+          calling <LinkText dark label="1-877-IDTHEFT" href="" />. To learn more
+          about identity theft protocol, visit the{" "}
+          <LinkText
+            dark
+            label="FTC’s Identity Theft site"
+            href="/#wow-modal-id-15"
+          />
+          .
+        </em>
+      </div>
     </InnerContent>
-  )
-}
+  );
+};
 
 export default IdTheft;
--- app/(pages)/solutions/remote-banking/RemoteBanking.tsx
+++ app/(pages)/solutions/remote-banking/RemoteBanking.tsx
@@ -2,7 +2,7 @@ import { remoteItems } from "./items";
 import List from "@/app/components/List";
 import ContentHeading from "@/app/components/inner-pages/ContentHeading";
 import ContentSecondaryHeading from "@/app/components/inner-pages/ContentSecondaryHeading";
-import InnerContent from "@/app/components/inner-pages/InnerContent"
+import InnerContent from "@/app/components/inner-pages/InnerContent";
 import ContentTertHeader from "@/app/components/inner-pages/ContentTertHeader";
 import ContentHeaderWrapper from "@/app/components/inner-pages/ContentHeaderWrapper";
 import Disclaimer from "@/app/components/Disclaimer";
@@ -22,39 +22,55 @@ const RemoteBanking = () => {
     >
       <ContentHeaderWrapper>
         <ContentHeading title="Remote Banking" />
-        <ContentSecondaryHeading italic title="24/7 Access enables life without interruptions!" />
+        <ContentSecondaryHeading
+          italic
+          title="24/7 Access enables life without interruptions!"
+        />
       </ContentHeaderWrapper>
       <ContentTertHeader label="Online & Mobile Banking" />
       <List liItems={remoteItems} />
       <div className="tw-text-lg tw-space-y-2">
-        <strong>Bill Pay Member Service</strong>: <LinkText label="844.596.1874" href="" /> between 7:00 AM - 1:00 AM ET, 7 days a week
-        <div><strong>PopMoney Support</strong>: <LinkText label="866.820.9920" href="" /> (option 2)</div>
+        <strong>Bill Pay Member Service</strong>:{" "}
+        <LinkText label="844.596.1874" href="tel:+1844-596-1874" /> between 7:00
+        AM - 1:00 AM ET, 7 days a week
+        <div>
+          <strong>PopMoney Support</strong>:{" "}
+          <LinkText label="866.820.9920" href="tel:+1866-820-9920" /> (option 2)
+        </div>
       </div>
       <div className="tw-text-lg">
-        <strong>Enroll in online banking</strong> and download our mobile app (Search “Mutual Savings Credit Union”) today to get access on all your devices!
+        <strong>Enroll in online banking</strong> and download our mobile app
+        (Search “Mutual Savings Credit Union”) today to get access on all your
+        devices!
       </div>
       <div className="tw-flex tw-items-center tw-justify-center tw-gap-4">
         <a href="">
-          <img className="tw-w-full tw-max-w-[167px]" src="http://dev2.growthbydesign.org/wp-content/uploads/app-store-logo.png" />
+          <img
+            className="tw-w-full tw-max-w-[167px]"
+            src="http://dev2.growthbydesign.org/wp-content/uploads/app-store-logo.png"
+          />
         </a>
         <a href="">
-          <img className="tw-w-full tw-max-w-[167px] tw-rounded-2xl" src="http://dev2.growthbydesign.org/wp-content/uploads/Google-Play-Emblema.png" />
+          <img
+            className="tw-w-full tw-max-w-[167px] tw-rounded-2xl"
+            src="http://dev2.growthbydesign.org/wp-content/uploads/Google-Play-Emblema.png"
+          />
         </a>
       </div>
       <div>
         <em>
-          To locate the MSCU mobile app, search for “Mutual Savings Credit Union Atlanta.”
+          To locate the MSCU mobile app, search for “Mutual Savings Credit Union
+          Atlanta.”
         </em>
       </div>
       <Disclaimer text="*Mobile Deposits have a 3-business day hold once posted to your account. Mobile Deposit limits are 10 items per month or a total of $5,000, whichever comes first, with a daily limit of 5 items and $1,500." />
       <ButtonWrapper>
-        <Button btnText="ATM Locator " link="https://www.culiance.com/en/atmlocator " />
-        <Button btnText="Shared Branching " link="https://co-opcreditunions.org/locator/?ref=allco-op.org&sc=1 " />
-        {/* <Button btnText="Audio Response 800.835.2815" link="tel+:800-835-2815" /> */}
+        <Button btnText="ATM Locator" link="/#wow-modal-id-1" />
+        <Button btnText="Shared Branching " link="/#wow-modal-id-11" />
       </ButtonWrapper>
-      <LinkText label="Audio Response 800.835.2815" href="tel:+800-835-2815" />
+      <LinkText label="Audio Response 800.835.2815" href="tel:+1800-835-2815" />
     </InnerContent>
-  )
-}
+  );
+};
 
 export default RemoteBanking;
