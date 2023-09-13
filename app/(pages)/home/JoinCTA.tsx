c2ea0f645ee36d4ad672062cfe522b3b5e623a8b
dillonzellis <dillonzellis@gmail.com>
2023-09-13 13:40:21 -0400

    make content changes to the join section

--- app/(pages)/home/JoinCTA.tsx
+++ app/(pages)/home/JoinCTA.tsx
@@ -1,3 +1,4 @@
+import Button from "@/app/components/Button";
 import Container from "@/app/layouts/Container";
 import ContentSection from "@/app/layouts/ContentSection";
 
@@ -8,11 +9,21 @@ const JoinCTA = () => {
         <div className="tw-flex tw-justify-center">
           <div className="tw-flex tw-max-w-prose tw-flex-col tw-gap-3 lg:tw-gap-6 tw-text-center">
             <div className="tw-text-2xl md:tw-text-3xl lg:tw-text-4xl tw-text-brandingBlue-400 tw-font-bold">
-              Learn how to join MSCU today!
+              Not a member yet?
             </div>
             <div className="tw-text-lg">
-              Join MSCU and use all of the free services that MSCU offers its
-              members such as checking, home banking and bill pay services
+              As a member of MSCU, you’ll not only enjoy lifetime membership in
+              the credit union, but you can access lower loan rates, higher
+              savings rates, and all the digital banking solution you need.
+              Plus, your friends and family members will also become eligible to
+              join. So, what are you waiting for?
+            </div>
+            <div className="tw-flex tw-justify-center tw-max-w-[200px] tw-w-full tw-mx-auto">
+              <Button
+                btnText="Join Now!"
+                link="https://dev2.growthbydesign.org/membership-application/"
+                fullWidth
+              />
             </div>
           </div>
         </div>
