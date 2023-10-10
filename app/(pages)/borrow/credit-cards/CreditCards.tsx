c68874d03c192e967a22b1cde435868aa705e070
dillonzellis <dillonzellis@gmail.com>
2023-10-10 12:39:42 -0400

    update plat cc section

--- app/(pages)/borrow/credit-cards/CreditCards.tsx
+++ app/(pages)/borrow/credit-cards/CreditCards.tsx
@@ -6,27 +6,30 @@ import ContentSecondaryHeading from "@/app/components/inner-pages/ContentSeconda
 import InnerContent from "@/app/components/inner-pages/InnerContent";
 import creditItems from "./items";
 import Disclaimer from "@/app/components/Disclaimer";
+import Container from "@/app/layouts/Container";
 
 const CreditCards = () => {
   return (
-    <InnerContent
-      id="credit-cards"
-      ImgSrc="http://dev2.growthbydesign.org/wp-content/uploads/iStock-1460236858.jpg"
-      ImgAlt=""
-      order="imageFirst"
-      variant="white"
-    >
-      <ContentHeaderWrapper>
-        <ContentHeading title="VISA® Rewards Platinum Credit Cards" />
-        <ContentSecondaryHeading
-          italic
-          title="All the convenience, not all the fees."
-        />
-      </ContentHeaderWrapper>
-      <List liItems={creditItems} />
-      <Button btnText="apply for a credit card" link="/#wow-modal-id-10" />
-      <Disclaimer text="*All loans subject to credit approval. Membership, terms, and conditions apply. | **Insurance and Warranty products are not products of the credit union; and are not obligations of or guaranteed by the credit union. Insurance or warranties may be purchased from an agent or an insurance company of the member’s choice; and the credit union makes no representations as to the services of any provider." />
-    </InnerContent>
+    <>
+      <InnerContent
+        id="credit-cards"
+        ImgSrc="http://dev2.growthbydesign.org/wp-content/uploads/iStock-1460236858.jpg"
+        ImgAlt=""
+        order="imageFirst"
+        variant="white"
+      >
+        <ContentHeaderWrapper>
+          <ContentHeading title="VISA® Platinum Rewards Credit Cards" />
+          <ContentSecondaryHeading
+            italic
+            title="All the convenience, not all the fees."
+          />
+        </ContentHeaderWrapper>
+        <List liItems={creditItems} />
+        <Button btnText="apply for a credit card" link="/#wow-modal-id-10" />
+        <Disclaimer text="*All loans subject to credit approval. Membership, terms, and conditions apply." />
+      </InnerContent>
+    </>
   );
 };
 
