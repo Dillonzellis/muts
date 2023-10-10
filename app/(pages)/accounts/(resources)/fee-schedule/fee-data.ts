25ed7dda2fc905a9ce554c38a37954245491acdc
dillonzellis <dillonzellis@gmail.com>
2023-10-10 12:38:31 -0400

    update fee table

--- app/(pages)/accounts/(resources)/fee-schedule/fee-data.ts
+++ app/(pages)/accounts/(resources)/fee-schedule/fee-data.ts
@@ -1,139 +1,157 @@
+export const memberFee = [
+  {
+    key: "memberFee",
+    label: "Mutual Savings CU Fee Schedule - Effective as of January 1, 2021",
+    className: "tw-rounded-t-xl",
+  },
+];
+
+export const memberData = [
+  {
+    fee: "Membership fee",
+    amount: "$5.00 per value",
+  },
+];
+
 export const feeHeaders = [
   {
-    key: 'fee',
-    label: 'Mutual Savings Cu Fee Schedule - 1/01/21',
-    className: 'tw-rounded-t-xl'
-  }
+    key: "fee",
+    label: "Deposit Account Fees",
+    className: "tw-rounded-t-xl",
+  },
 ];
 
 export const feeData = [
   {
-    fee: 'Nonsufficient Funds (NSF)',
-    amount: '$30.00 per item',
+    fee: "Nonsufficient Funds (NSF)",
+    amount: "$30.00 per item",
   },
   {
-    fee: 'Courtesy Pay Fee',
-    amount: '$30.00 per item',
+    fee: "Courtesy Pay Fee",
+    amount: "$30.00 per item",
   },
   {
-    fee: 'Stop Payments',
-    amount: '$25.00 per item',
+    fee: "Stop Payments",
+    amount: "$25.00 per item",
   },
   {
-    fee: 'Copy of Paid Drafts',
-    amount: '$3.00 per copy',
+    fee: "Copy of Paid Drafts",
+    amount: "$3.00 per copy",
   },
   {
-    fee: 'ACH Stop Payment',
-    amount: '$25.00 per item',
+    fee: "ACH Stop Payment",
+    amount: "$25.00 per item",
   },
   {
-    fee: 'Return Deposit Items',
-    amount: '15.00 per item',
+    fee: "Return Deposit Items",
+    amount: "15.00 per item",
   },
   {
-    fee: 'ATM Transactions',
-    amount: 'Free*',
+    fee: "ATM Transactions",
+    amount: "Free*",
   },
-]
+  {
+    fee: "IRA Transfers to Other Institutions",
+    amount: "$15.00 per transfer",
+  },
+];
 
 export const mmHeaders = [
   {
-    key: 'fee',
-    label: 'Money Market Account Fees',
-    className: 'tw-rounded-t-xl'
-  }
-]
+    key: "fee",
+    label: "Money Market Account Fees",
+    className: "tw-rounded-t-xl",
+  },
+];
 
 export const mmData = [
   {
-    fee: 'All other fees applicable to Deposit Accounts (example: NSF, Stop Payments, Returned Deposits, etc.)'
-  }
-]
+    fee: "All other fees applicable to Deposit Accounts (example: NSF, Stop Payments, Returned Deposits, etc.)",
+  },
+];
 
 export const webHeaders = [
   {
-    key: 'fee',
+    key: "fee",
     label: "Web Bill Pay Fees",
-    className: 'tw-rounded-t-xl'
-  }
-]
+    className: "tw-rounded-t-xl",
+  },
+];
 
 export const webData = [
   {
-    fee: 'NSF/Overdraft',
-    amount: '$30.00 per item',
+    fee: "NSF/Overdraft",
+    amount: "$30.00 per item",
   },
   {
-    fee: 'Card Replacement Fee',
-    amount: '$25.00 per item',
+    fee: "Card Replacement Fee",
+    amount: "$25.00 per item",
   },
   {
     fee: 'Bill Pay "Rush Delivery"',
-    amount: '$9.95 per item',
+    amount: "$9.95 per item",
   },
-]
+];
 
 export const otherHeaders = [
   {
-    key: 'fee',
-    label: 'Other Services Fees(Applicable to all Accounts)',
-    className: 'tw-rounded-t-xl'
-  }
-]
+    key: "fee",
+    label: "Other Services Fees(Applicable to all Accounts)",
+    className: "tw-rounded-t-xl",
+  },
+];
 
 export const otherData = [
   {
-    fee: 'Account Reconciliation',
-    amount: '$20.00 per hour'
+    fee: "Account Reconciliation",
+    amount: "$20.00 per hour",
   },
   {
-    fee: 'Card Replacement Fee',
-    amount: '1 Free per year; $5.00 per card thereafter',
+    fee: "Card Replacement Fee",
+    amount: "1 Free per year; $5.00 per card thereafter",
   },
   {
-    fee: 'Rushed Debit Cards',
-    amount: '$35.00 ',
+    fee: "Rushed Debit Cards",
+    amount: "$35.00 ",
   },
   {
-    fee: 'Official Checks',
-    amount: '$3.00 per check',
+    fee: "Official Checks",
+    amount: "$3.00 per check",
   },
   {
-    fee: 'Express Mail Fee',
-    amount: 'At Cost',
+    fee: "Express Mail Fee",
+    amount: "At Cost",
   },
   {
-    fee: 'Gift Card Fee',
-    amount: '$2.50 each',
+    fee: "Gift Card Fee",
+    amount: "$2.50 each",
   },
   {
-    fee: 'Credit Union Checks',
-    amount: '$1.00 per Check',
+    fee: "Credit Union Checks",
+    amount: "$1.00 per Check",
   },
   {
-    fee: 'Money Orders',
-    amount: '$1.00 per Money Order',
+    fee: "Money Orders",
+    amount: "$1.00 per Money Order",
   },
   {
-    fee: 'Research Fee',
-    amount: '$20.00 per hours',
+    fee: "Research Fee",
+    amount: "$20.00 per hours",
   },
   {
-    fee: 'Statement Copy',
-    amount: '$2.00 per hard copy',
+    fee: "Statement Copy",
+    amount: "$2.00 per hard copy",
   },
   {
-    fee: 'Incoming Domestic Wire',
-    amount: '$2.00 per wire',
+    fee: "Incoming Domestic Wire",
+    amount: "$2.00 per wire",
   },
   {
-    fee: 'Outgoing Domestic Wire',
-    amount: '$15.00 per wire',
+    fee: "Outgoing Domestic Wire",
+    amount: "$15.00 per wire",
   },
   {
-    fee: 'Garnishment & Levy Processing Fee',
-    amount: '10% of account balance up to $50.00 maximum',
+    fee: "Garnishment & Levy Processing Fee",
+    amount: "10% of account balance up to $50.00 maximum",
   },
-]
-
+];
--- app/(pages)/accounts/(resources)/fee-schedule/page.tsx
+++ app/(pages)/accounts/(resources)/fee-schedule/page.tsx
@@ -1,5 +1,16 @@
 import SectionContent from "@/app/components/inner-pages/SectionContent";
-import { feeHeaders, feeData, mmHeaders, mmData, webHeaders, webData } from "./fee-data";
+import {
+  feeHeaders,
+  feeData,
+  mmHeaders,
+  mmData,
+  webHeaders,
+  webData,
+  memberFee,
+  memberData,
+  otherHeaders,
+  otherData,
+} from "./fee-data";
 import DataTable from "@/app/components/DataTable";
 import Container from "@/app/layouts/Container";
 import Disclaimer from "@/app/components/Disclaimer";
@@ -11,26 +22,20 @@ const FeeSchedule = () => {
       <Container>
         <PageHeader title="Fee Schedule" />
         <div className="tw-flex tw-flex-col tw-gap-8">
-          <DataTable
-            headers={feeHeaders}
-            data={feeData}
-          />
-          <DataTable
-            headers={mmHeaders}
-            data={mmData}
-          />
-          <DataTable
-            headers={webHeaders}
-            data={webData}
-          />
-          <div>
-            <Disclaimer text="*Members will receive all ATM Transactions at no charge. CULIANCE and Publix Presto ATM transactions are permitted unlimited transactions at no additional charge." />
+          <DataTable headers={memberFee} data={memberData} />
+          <DataTable headers={feeHeaders} data={feeData} />
+          <DataTable headers={mmHeaders} data={mmData} />
+          <DataTable headers={webHeaders} data={webData} />
+          <DataTable headers={otherHeaders} data={otherData} />
+          <div className="tw-space-y-4">
             <Disclaimer text="The fees appearing in this Schedule are accurate and effective for member accounts as of the Effective Date on this Fee Schedule. If you have any questions or require current rate and fee information on your accounts, please contact a credit union representative for assistance." />
+            <Disclaimer text="*To be a member of MSCU in good standing, you must maintain a par value of $5.00 in your primary share savings account at all times. If you should fall below this minimum required balance, your account will be subject to account suspension, revocation, or membership privileges, and/or possible account closure." />
+            <Disclaimer text="**Members will receive all ATM Transactions at no charge. CULIANCE and Publix Presto ATM transactions are permitted unlimited transactions at no additional charge." />
           </div>
         </div>
       </Container>
-    </SectionContent >
-  )
-}
+    </SectionContent>
+  );
+};
 
 export default FeeSchedule;
